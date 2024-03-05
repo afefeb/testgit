1fa3a18a602c960ee04529441a72e217d159961a
afefeb <afefebryan@gmail.com>
2024-03-05 10:45:41 +0700

    nambah baris

---
+++ haha.java
@@ -0,0 +1,7 @@
+public class haha{
+    public static void main(String[] args) {
+        System.out.println("123");
+        System.out.println("hehe");
+        System.out.println("huhu");
+    }
+}
